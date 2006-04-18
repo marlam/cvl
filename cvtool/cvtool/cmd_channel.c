e91d447a29e80871cb5d3bae21a97387487f3c60
Martin Lambers <marlam@marlam.de>
2006-04-18 15:01:55 +0000

    cvtool: Add option to the channel command to extract a channel as a graylevel image.

--- cvtool/cvtool/cmd_channel.c
+++ cvtool/cvtool/cmd_channel.c
@@ -34,18 +34,21 @@
 void cmd_channel_print_help(void)
 {
     cvl_msg_fmt_req(
-	    "channel -c|--channel=r|g|b\n"
+	    "channel [-g|--gray] -c|--channel=r|g|b\n"
 	    "\n"
-	    "Pick the given channel from the input.");
+	    "Pick the given channel from the input. The default is to keep the color of the channel. "
+	    "If --gray is used, then the channel will be extracted into graylevel frames instead.");
 }
 
 
 int cmd_channel(int argc, char *argv[])
 {
+    option_bool_t gray = { false, true };
     const char *channel_names[] = { "r", "g", "b", NULL };
     option_name_t channel = { -1, channel_names };
     option_t options[] = 
     { 
+	{ "gray",    'g', OPTION_BOOL, &gray,    false },
 	{ "channel", 'c', OPTION_NAME, &channel, true },
 	null_option 
     };
@@ -72,17 +75,45 @@ int cmd_channel(int argc, char *argv[])
 	    error = true;
 	    break;
 	}
-	if (channel.value == 0)
-	{	
-	    cvl_frame_rgb_to_r(frame);
-	}
-	else if (channel.value == 1)
+	cvl_frame_to_rgb(frame);
+	for (int i = 0; i < cvl_frame_width(frame) * cvl_frame_height(frame); i++)
 	{
-	    cvl_frame_rgb_to_g(frame);
+	    cvl_pixel_t p = cvl_frame_get_i(frame, i);
+	    if (gray.value)
+	    {
+		if (channel.value == 0)
+		{
+		    p = cvl_pixel_gray_to_rgb(cvl_pixel_rgb_to_r(p));
+		}
+		else if (channel.value == 1)
+		{
+		    p = cvl_pixel_gray_to_rgb(cvl_pixel_rgb_to_g(p));
+		}
+		else
+		{
+		    p = cvl_pixel_gray_to_rgb(cvl_pixel_rgb_to_b(p));
+		}
+	    }
+	    else
+	    {
+		if (channel.value == 0)
+		{
+		    p = cvl_pixel_rgb(cvl_pixel_rgb_to_r(p), 0, 0);
+		}
+		else if (channel.value == 1)
+		{
+		    p = cvl_pixel_rgb(0, cvl_pixel_rgb_to_r(p), 0);
+		}
+		else
+		{
+		    p = cvl_pixel_rgb(0, 0, cvl_pixel_rgb_to_r(p));
+		}
+	    }
+	    cvl_frame_set_i(frame, i, p);
 	}
-	else
+	if (gray.value)
 	{
-	    cvl_frame_rgb_to_b(frame);
+	    cvl_frame_set_pixel_type(frame, CVL_PIXEL_GRAY);
 	}
 	if (!cvl_io_write(stdout, output_info, frame))
 	{
