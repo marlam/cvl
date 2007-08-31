bff5ab7277835e00f6727f6e3e3556acfcb13e46
Martin Lambers <marlam@marlam.de>
2007-08-31 14:14:51 +0000

    Move to new CVL/cvtool 0.2.0 code base.

--- cvtool/tests/cmd_help.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-. cmd_tests_common.sh
-
-$CVTOOL help 		2> help.txt 	
-$CVTOOL help help 	2> helphelp.txt	
-
-cmd_tests_cleanup
--- cvtool/tests/cmd_version.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-. cmd_tests_common.sh
-
-$CVTOOL version 2> version.txt 
-
-cmd_tests_cleanup
