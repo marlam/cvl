3d08d56488f12ec7644516f0a9e38e251ff9604a
Martin Lambers <marlam@marlam.de>
2008-01-16 16:46:40 +0000

    Add the methods reinhard05, ashikhmin02, reinhard02 to the tonemap command.

--- cvtool/cvtool/cmd_tonemap.c
+++ cvtool/cvtool/cmd_tonemap.c
@@ -38,23 +38,48 @@ void cmd_tonemap_print_help(void)
 	    "tonemap -m|--method=tumblin99 [-l|--max-absolute-luminance=<l>] "
 	    "[--display-adaptation-level=<d>] [--max-displayable-contrast=<c>]\n"
 	    "tonemap -m|--method=drago03 [-l|--max-absolute-luminance=<l>] [--max-display-luminance=<d>] [--bias=<b>]\n"
+	    "tonemap -m|--method=reinhard05 [--intensity=<i>] [--light-adaptation=<l>] [--chromatic-adaptation=<c>]\n"
+	    "tonemap -m|--method=ashikhmin02 [-l|--max-absolute-luminance=<l>] [--local-contrast=<c>]\n"
 	    "tonemap -m|--method=durand02 [-l|--max-absolute-luminance=<l>] [--sigma-spatial=<ss>] [--sigma-luminance=<sl>] [--base-contrast=<bc>]\n"
+	    "tonemap -m|--method=reinhard02 [--key-value=<a>] [--white=<w>] [--sharpness=<s>] [--epsilon=<e>]\n"
 	    "\n"
 	    "Tone map frames. High dynamic range (HDR) frames are read from standard input, and low dynamic range (LDR) frames "
-	    "are written to standard output.\n"
+	    "are written to standard output. For some methods, the results should be gamma corrected.\n"
 	    "See the original papers for a description of the parameters.\n"
 	    "The default for the maximum absolute luminance is to get it from the file (if specified), or else 150.0.\n"
 	    "The default for schlick94 is b=100.0.\n"
 	    "The defaults for tumblin99 are d=100.0, c=70.0.\n"
 	    "The defaults for drago03 are d=200.0, b=0.85.\n"
-	    "The defaults for durand02 are ss=0.3, sl=0.4, bc=2.0. The results of this method need to be gamma corrected!");
+	    "The defaults for reinhard05 are i=0.0, l=0.5, c=0.5.\n"
+	    "The default for ashikhmin02 is c=0.5.\n"
+	    "The defaults for durand02 are ss=0.3, sl=0.4, bc=2.0.\n"
+	    "The defaults for reinhard02 are a=0.1, w=1.0, s=10.0, e=0.5.");
 }
 
 
 int cmd_tonemap(int argc, char *argv[])
 {
-    typedef enum { TM_SCHLICK94, TM_TUMBLIN99, TM_DRAGO03, TM_DURAND02 = 0 } method_t;
-    const char *method_names[] = { "schlick94", "tumblin99", "drago03", "durand02" , NULL };
+    typedef enum 
+    { 
+	TM_SCHLICK94, 
+	TM_TUMBLIN99, 
+	TM_DRAGO03, 
+	TM_REINHARD05, 
+	TM_ASHIKHMIN02,
+	TM_DURAND02,
+	TM_REINHARD02
+    } method_t;
+    const char *method_names[] = 
+    { 
+	"schlick94", 
+	"tumblin99", 
+	"drago03", 
+	"reinhard05", 
+	"ashikhmin02",
+	"durand02", 
+	"reinhard02", 
+	NULL 
+    };
     mh_option_name_t method = { -1, method_names };
     mh_option_float_t max_abs_lum = { -1.0f, 0.0f, false, FLT_MAX, true };
     mh_option_float_t schlick94_p = { 100.0f, 1.0f, true, FLT_MAX, true };
@@ -62,21 +87,37 @@ int cmd_tonemap(int argc, char *argv[])
     mh_option_float_t tr99_max_contrast = { 70.0f, 0.0f, false, FLT_MAX, true };
     mh_option_float_t drago03_max_disp_lum = { 200.0f, 0.0f, false, FLT_MAX, true };
     mh_option_float_t drago03_bias = { 0.85f, 0.0f, true, 1.0f, true };
+    mh_option_float_t reinhard05_intensity = { 0.0f, -8.0f, true, 8.0f, true };
+    mh_option_float_t reinhard05_light_adapt = { 0.5f, 0.0f, true, 1.0f, true };
+    mh_option_float_t reinhard05_chroma_adapt = { 0.5f, 0.0f, true, 1.0f, true };
+    mh_option_float_t ashikhmin02_local_contrast = { 0.5f, 0.0f, true, 10.0f, true };
     mh_option_float_t durand02_sigma_spatial = { 0.3f, 0.0f, false, FLT_MAX, true };
     mh_option_float_t durand02_sigma_luminance = { 0.4f, 0.0f, false, FLT_MAX, true };
     mh_option_float_t durand02_base_contrast = { 3.0f, 1.0f, false, FLT_MAX, true };
+    mh_option_float_t reinhard02_key_value = { 0.1f, 0.0f, false, 1.0f, true };
+    mh_option_float_t reinhard02_white = { 1.0f, 0.0f, true, 100.0f, false };
+    mh_option_float_t reinhard02_sharpness = { 10.0f, 0.0f, true, 100.0f, false };
+    mh_option_float_t reinhard02_epsilon = { 0.5f, 0.0f, true, 1.0f, true };
     mh_option_t options[] = 
     {
-	{ "method",                    'm', MH_OPTION_NAME,  &method,                   true  },
-	{ "max-absolute-luminance",    'l', MH_OPTION_FLOAT, &max_abs_lum,              false },
-	{ "brightness",               '\0', MH_OPTION_FLOAT, &schlick94_p,              false },
-	{ "display-adaptation-level", '\0', MH_OPTION_FLOAT, &tr99_disp_adapt_level,    false },
-	{ "max-displayable-contrast", '\0', MH_OPTION_FLOAT, &tr99_max_contrast,        false },
-	{ "max-display-luminance",    '\0', MH_OPTION_FLOAT, &drago03_max_disp_lum,     false },
-	{ "bias",                     '\0', MH_OPTION_FLOAT, &drago03_bias,             false },
-	{ "sigma-spatial",            '\0', MH_OPTION_FLOAT, &durand02_sigma_spatial,   false },
-	{ "sigma-luminance" ,         '\0', MH_OPTION_FLOAT, &durand02_sigma_luminance, false },
-	{ "base-contrast",            '\0', MH_OPTION_FLOAT, &durand02_base_contrast,   false },
+	{ "method",                    'm', MH_OPTION_NAME,  &method,                     true  },
+	{ "max-absolute-luminance",    'l', MH_OPTION_FLOAT, &max_abs_lum,                false },
+	{ "brightness",               '\0', MH_OPTION_FLOAT, &schlick94_p,                false },
+	{ "display-adaptation-level", '\0', MH_OPTION_FLOAT, &tr99_disp_adapt_level,      false },
+	{ "max-displayable-contrast", '\0', MH_OPTION_FLOAT, &tr99_max_contrast,          false },
+	{ "max-display-luminance",    '\0', MH_OPTION_FLOAT, &drago03_max_disp_lum,       false },
+	{ "bias",                     '\0', MH_OPTION_FLOAT, &drago03_bias,               false },
+	{ "intensity",                '\0', MH_OPTION_FLOAT, &reinhard05_intensity,       false },
+	{ "light-adaptation",         '\0', MH_OPTION_FLOAT, &reinhard05_light_adapt,     false },
+	{ "chromatic-adaptation",     '\0', MH_OPTION_FLOAT, &reinhard05_chroma_adapt,    false },
+	{ "local-contrast",           '\0', MH_OPTION_FLOAT, &ashikhmin02_local_contrast, false },
+	{ "sigma-spatial",            '\0', MH_OPTION_FLOAT, &durand02_sigma_spatial,     false },
+	{ "sigma-luminance" ,         '\0', MH_OPTION_FLOAT, &durand02_sigma_luminance,   false },
+	{ "base-contrast",            '\0', MH_OPTION_FLOAT, &durand02_base_contrast,     false },
+	{ "key-value",                '\0', MH_OPTION_FLOAT, &reinhard02_key_value,       false },
+	{ "white",                    '\0', MH_OPTION_FLOAT, &reinhard02_white,           false },
+	{ "sharpness",                '\0', MH_OPTION_FLOAT, &reinhard02_sharpness,       false },
+	{ "epsilon",                  '\0', MH_OPTION_FLOAT, &reinhard02_epsilon,         false },
 	mh_option_null
     };
     cvl_stream_type_t stream_type;
@@ -143,6 +184,41 @@ int cmd_tonemap(int argc, char *argv[])
 	    cvl_tonemap_drago03(tonemapped_frame, frame, max_abs_lum.value, 
 		    drago03_bias.value, drago03_max_disp_lum.value);
 	}
+	else if (method.value == TM_REINHARD05)
+	{
+  	    float min_lum;
+  	    float avg_lum;
+ 	    float log_avg_lum;
+	    float channel_avg[4];
+	    cvl_frame_t *tmpframe = cvl_frame_new(cvl_frame_width(frame), cvl_frame_height(frame),
+		    3, CVL_RGB, CVL_FLOAT, CVL_TEXTURE);
+	    cvl_reduce(frame, CVL_REDUCE_MIN, 1, &min_lum);
+	    cvl_reduce(frame, CVL_REDUCE_SUM, 1, &avg_lum);
+	    avg_lum /= (float)cvl_frame_size(frame);
+	    log_avg_lum = cvl_log_avg_lum(frame, tmpframe, 1.0f);
+	    cvl_convert_format(tmpframe, frame);
+	    cvl_reduce(tmpframe, CVL_REDUCE_SUM, -1, channel_avg);
+	    channel_avg[0] /= (float)cvl_frame_size(frame);
+	    channel_avg[1] /= (float)cvl_frame_size(frame);
+	    channel_avg[2] /= (float)cvl_frame_size(frame);
+            cvl_tonemap_reinhard05(tonemapped_frame, frame,
+		    min_lum, avg_lum, log_avg_lum, tmpframe, channel_avg,
+		    reinhard05_intensity.value,
+		    reinhard05_chroma_adapt.value,
+		    reinhard05_light_adapt.value);
+	    cvl_frame_free(tmpframe);
+	}
+	else if (method.value == TM_ASHIKHMIN02)
+	{
+	    cvl_frame_t *tmpframe = cvl_frame_new(cvl_frame_width(frame), cvl_frame_height(frame),
+		    4, CVL_UNKNOWN, CVL_FLOAT, CVL_TEXTURE);
+	    float min_abs_lum;
+	    cvl_reduce(frame, CVL_REDUCE_MIN, 1, &min_abs_lum);
+	    min_abs_lum *= max_abs_lum.value;
+	    cvl_tonemap_ashikhmin02(tonemapped_frame, frame,
+	    	    min_abs_lum, max_abs_lum.value, tmpframe, ashikhmin02_local_contrast.value);
+	    cvl_frame_free(tmpframe);
+	}
 	else if (method.value == TM_DURAND02)
 	{
 	    cvl_frame_t *tmp = cvl_frame_new(cvl_frame_width(frame), cvl_frame_height(frame), 
@@ -154,6 +230,16 @@ int cmd_tonemap(int argc, char *argv[])
 		    durand02_sigma_luminance.value,
 		    durand02_base_contrast.value);
 	    cvl_frame_free(tmp);
+	}
+	else if (method.value == TM_REINHARD02)
+	{
+	    cvl_frame_t *tmpframe = cvl_frame_new(cvl_frame_width(frame), cvl_frame_height(frame),
+		    4, CVL_UNKNOWN, CVL_FLOAT, CVL_TEXTURE);
+	    float log_avg_lum = cvl_log_avg_lum(frame, tmpframe, 1.0f);
+	    cvl_tonemap_reinhard02(tonemapped_frame, frame, tmpframe, log_avg_lum,
+		    reinhard02_key_value.value, reinhard02_white.value, 
+		    reinhard02_sharpness.value, reinhard02_epsilon.value);
+	    cvl_frame_free(tmpframe);
 	}
 	cvl_frame_free(frame);
 	cvl_convert_format_inplace(tonemapped_frame, format);
--- cvtool/tests/cmd_tonemap.sh
+++ cvtool/tests/cmd_tonemap.sh
@@ -6,7 +6,10 @@ $CVTOOL create -w 10 -h 10 -c red > r.pnm
 
 $CVTOOL tonemap -m schlick94 --brightness 100.0 < r.pnm > /dev/null
 $CVTOOL tonemap -m tumblin99 --max-display-luminance=200.0 --display-adaptation-level=100.0 --max-displayable-contrast=70.0 < r.pnm > /dev/null
+$CVTOOL tonemap -m reinhard05 --intensity=0.1 --light-adaptation=0.5 --chromatic-adaptation=0.5 < r.pnm > /dev/null
+$CVTOOL tonemap -m ashikhmin02 --local-contrast=0.5 < r.pnm > /dev/null
 $CVTOOL tonemap -m drago03 --max-display-luminance=200.0 --bias=0.84 < r.pnm > /dev/null
 $CVTOOL tonemap -m durand02 --sigma-spatial=0.3 --sigma-luminance=0.4 --base-contrast=3 < r.pnm > /dev/null
+$CVTOOL tonemap -m reinhard02 --key-value=0.1 --white=1.0 --sharpness=10.0 --epsilon=0.5 < r.pnm > /dev/null
 
 cmd_tests_cleanup
