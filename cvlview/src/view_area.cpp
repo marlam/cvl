fdf0f195f4574a273169653852a8a0decb4b1f7a
Martin Lambers <marlam@marlam.de>
2008-04-14 07:32:58 +0000

    Use GL_CLAMP_TO_EDGE instead of GL_CLAMP for textures.

--- cvlview/src/view_area.cpp
+++ cvlview/src/view_area.cpp
@@ -273,8 +273,8 @@ void ViewArea::paintGL()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     }
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glBegin(GL_QUADS);
     glTexCoord2f(0.0f, 0.0f);
     glVertex3f(-x + xo, +y - yo, 0.0f);
