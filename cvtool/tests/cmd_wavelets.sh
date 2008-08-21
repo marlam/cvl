7c10be3b6a290cd84ec9f3e871efc7fe57787963
Martin Lambers <marlam@marlam.de>
2008-08-21 08:05:45 +0000

    Fix test.

--- cvtool/tests/cmd_wavelets.sh
+++ cvtool/tests/cmd_wavelets.sh
@@ -4,14 +4,17 @@
 
 cmd_tests_init
 
-$CVTOOL create -w 10 -h 10 -c red -t float > red.pfs 
+$CVTOOL create -w 10 -h 10 -c red > red.ppm
+$CVTOOL convert -t float < red.ppm > red.pfs
 
 $CVTOOL wavelets -t dwt -D 2 -l 1 < red.pfs > dwt.pfs
 $CVTOOL wavelets -t idwt -D 2 -l 1 < dwt.pfs > xred.pfs
-cmp red.pfs xred.pfs
+$CVTOOL convert -t uint8 < xred.pfs > xred.ppm
+cmp red.ppm xred.ppm
 $CVTOOL wavelets -t soft-thresholding -l 1 -T 0 < dwt.pfs > st.pfs
 $CVTOOL wavelets -t hard-thresholding -l 1 -T 0 < dwt.pfs > ht.pfs
 $CVTOOL wavelets -t idwt -D 2 -l 1 < st.pfs > xxred.pfs
-cmp red.pfs xxred.pfs
+$CVTOOL convert -t uint8 < xxred.pfs > xxred.ppm
+cmp red.ppm xxred.ppm
 
 cmd_tests_cleanup
