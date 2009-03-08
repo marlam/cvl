adc7af991091ae019f1b152bafcc0419eef27610
Martin Lambers <marlam@marlam.de>
2009-03-08 12:53:24 +0100

    Bump version number and/or copyright year.

--- cvlview/src/view_area.h
+++ cvlview/src/view_area.h
@@ -3,7 +3,7 @@
  *
  * This file is part of cvlview, an image viewer using the CVL library.
  *
- * Copyright (C) 2007, 2008  Martin Lambers <marlam@marlam.de>
+ * Copyright (C) 2007, 2008, 2009  Martin Lambers <marlam@marlam.de>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
